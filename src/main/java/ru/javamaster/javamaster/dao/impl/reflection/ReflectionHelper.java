44a6f157740e7716131b439b4f1de5e99548da32
Фарид <85080879+Fara322@users.noreply.github.com>
2021-12-14 00:13:36 +0300

    Реализовал класс, ReflectionHelper.

--- src/main/java/ru/javamaster/javamaster/dao/impl/reflection/ReflectionHelper.java
+++ src/main/java/ru/javamaster/javamaster/dao/impl/reflection/ReflectionHelper.java
@@ -2,23 +2,22 @@ package ru.javamaster.javamaster.dao.impl.reflection;
 
 import java.lang.annotation.Annotation;
 
-public class ReflectionHelper {
+public class ReflectionHelper<T> {
 
-    boolean hasSuperClass(Class<?> pc) {
-//        if(pc.isInstance(pc)) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-
-        return pc.isInstance(pc);
+    public boolean hasSuperClass(Class<?> pc) {
+        return pc.getClass().isAssignableFrom(pc.getClass());
     }
 
-    boolean hasClassFieldWithAnnotation(Class<?> pc, Class<? extends Annotation> annotationClass) {
-        return false;
+    public boolean hasClassFieldWithAnnotation(Class<?> pc, Class<? extends Annotation> annotationClass) {
+        return pc.isAnnotationPresent(annotationClass);
     }
 
-//    public Class<? super T> getSuperClassWithAnnotation(Class pc, Class<? extends Annotation> annotationClass) {
-//        return null;
-//    }
+    public Class<? super T> getSuperClassWithAnnotation(Class pc, Class<? extends Annotation> annotationClass) {
+
+        if (pc.isAnnotationPresent(annotationClass)) {
+            return pc.getSuperclass();
+        } else {
+            return null;
+        }
+    }
 }
