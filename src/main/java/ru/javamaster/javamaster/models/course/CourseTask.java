aa413dd1ea1b2976a5fb905b4a8c9943321b56e7
vladi <vladimir.matveyev@outlook.com>
2021-12-16 20:00:30 +0300

    complete

---
+++ src/main/java/ru/javamaster/javamaster/models/course/CourseTask.java
@@ -0,0 +1,10 @@
+package ru.javamaster.javamaster.models.course;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CourseTask {
+    @Id
+    private Long id;
+}
