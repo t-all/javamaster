80d28f69e6980fcffb7b6d360ab4a408c5e6c745
vladi <vladimir.matveyev@outlook.com>
2021-12-16 22:57:49 +0300

    working

--- src/main/java/ru/javamaster/javamaster/dao/impl/reflection/ReflectionHelper.java
+++ src/main/java/ru/javamaster/javamaster/dao/impl/reflection/ReflectionHelper.java
@@ -7,23 +7,6 @@ import java.lang.reflect.Method;
 
 public class ReflectionHelper <T>{
 
-        public boolean hasSuperClass(Class<?> pc) {
-            return pc.getClass().isAssignableFrom(pc.getClass());
-        }
-
-        public boolean hasClassFieldWithAnnotation(Class<?> pc, Class<? extends Annotation> annotationClass) {
-            return pc.isAnnotationPresent(annotationClass);
-        }
-
-        public Class<? super T> getSuperClassWithAnnotation(Class pc, Class<? extends Annotation> annotationClass) {
-
-            if (pc.isAnnotationPresent(annotationClass)) {
-                return pc.getSuperclass();
-            } else {
-                return null;
-            }
-        }
-
         public Field getAnnotatedField(Class<?> pc, Class<? extends Annotation> annotationClass){
 
             return null;
