af90faaa958cf344fe66bf0a9c535bb54af685f0
vladi <vladimir.matveyev@outlook.com>
2021-12-19 01:34:01 +0300

    after closed

--- src/main/java/ru/javamaster/javamaster/dao/impl/reflection/ReflectionHelper.java
+++ src/main/java/ru/javamaster/javamaster/dao/impl/reflection/ReflectionHelper.java
@@ -1,16 +1,12 @@
 package ru.javamaster.javamaster.dao.impl.reflection;
 
 import java.lang.annotation.Annotation;
-<<<<<<< HEAD
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class ReflectionHelper <T> {
-=======
-
 public class ReflectionHelper<T> {
->>>>>>> dev
+
 
     public boolean hasSuperClass(Class<?> pc) {
         return pc.getClass().isAssignableFrom(pc.getClass());
@@ -28,7 +24,6 @@ public class ReflectionHelper<T> {
             return null;
         }
     }
-<<<<<<< HEAD
 
     public Field getAnnotatedField(Class<?> pc, Class<? extends Annotation> annotationClass){
 
@@ -59,51 +54,3 @@ public class ReflectionHelper<T> {
     }
 }
 
-//
-//
-//    public void setPosition(T entity, Class<?> pc, Integer position, String positionFieldName) {
-//        try {
-//            Field field = pc.getDeclaredField(positionFieldName);
-//            field.set(entity, position);
-//        } catch (NoSuchFieldException | IllegalAccessException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public <K, T> K getId(T entity, Class<?> pc, String idFieldName) {
-//        K id = null;
-//
-//        try {
-//            Field field = pc.getDeclaredField(idFieldName);
-//            id = (K) field.get(entity);
-//        } catch (NoSuchFieldException | IllegalAccessException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return id;
-//    }
-//
-//    public Integer getPosition(T entity, Class<?> pc, String positionFieldName) {
-//        Integer position = null;
-//
-//        try {
-//            Field field = pc.getDeclaredField(positionFieldName);
-//            position = (Integer) field.get(entity);
-//        } catch (NoSuchFieldException | IllegalAccessException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return position;
-//    }
-//
-//    public void setId(T entity, Class<?> pc, Long id) {
-//        try {
-//            Field field = pc.getDeclaredField("id");
-//            field.set(entity, id);
-//        } catch (NoSuchFieldException | IllegalAccessException e) {
-//            e.printStackTrace();
-//        }
-//    }
-=======
-}
->>>>>>> dev
