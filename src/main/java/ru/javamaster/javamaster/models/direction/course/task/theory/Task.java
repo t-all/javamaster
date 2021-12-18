5a50a79e13bd162a152c898e717cfdd28d3af61e
t-all <t-all@yandex.ru>
2021-12-18 14:32:35 +0300

    Theory Task

---
+++ src/main/java/ru/javamaster/javamaster/models/direction/course/task/theory/Task.java
@@ -0,0 +1,27 @@
+package ru.javamaster.javamaster.models.direction.course.task.theory;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+}
