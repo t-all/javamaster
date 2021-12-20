5bd3f9b4f2792f96c04b11d0aa75b78c91da2845
vladi <vladimir.matveyev@outlook.com>
2021-12-20 15:27:03 +0300

    Fixed

---
+++ src/main/java/ru/javamaster/javamaster/models/user/CourseTask.java
@@ -0,0 +1,11 @@
+package ru.javamaster.javamaster.models.user;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CourseTask {
+    @Id
+    private Long id;
+
+}
