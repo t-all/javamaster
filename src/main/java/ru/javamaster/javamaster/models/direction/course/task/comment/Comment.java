1192dd266b4663b7fa4ae0dc2243778e9a76315c
t-all <t-all@yandex.ru>
2021-12-20 12:20:55 +0300

    Comment

---
+++ src/main/java/ru/javamaster/javamaster/models/direction/course/task/comment/Comment.java
@@ -0,0 +1,69 @@
+package ru.javamaster.javamaster.models.direction.course.task.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.Type;
+import ru.javamaster.javamaster.models.User;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@EqualsAndHashCode(of = {"id", "text", "user"})
+@ToString(of = {"id", "text", "user"})
+@Table(name = "comment")
+
+public abstract class Comment implements Comparable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Lob
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private boolean isMainComment = false;
+
+    @Transient
+    private List<DependentComments> dependentComments = new LinkedList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment myMainComment;
+
+    private Date date = new Date();
+
+
+    @Override
+    public int compareTo(Comment o) {
+        return getDate().compareTo(o.getDate());
+    }
+
+}
