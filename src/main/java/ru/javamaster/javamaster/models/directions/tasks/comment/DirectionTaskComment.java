3ea4c26721f089bfe742f4577d9bea7db345c7aa
stalkervr <stalkervr@gmail.com>
2021-12-21 21:35:31 +0700

    Corrected by comments. Delete reference in plug class DirectionTaskComment. Delete unused imports

--- src/main/java/ru/javamaster/javamaster/models/directions/tasks/comment/DirectionTaskComment.java
+++ src/main/java/ru/javamaster/javamaster/models/directions/tasks/comment/DirectionTaskComment.java
@@ -7,15 +7,11 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import ru.javamaster.javamaster.models.directions.tasks.DirectionTask;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 
