db4d2b4d84419d7d516e488625318ac0ebd6b19a
Фарид <85080879+Fara322@users.noreply.github.com>
2021-12-23 16:14:48 +0300

    Создал класс InviteToken, настроил связь между таблицами.

---
+++ src/main/java/ru/javamaster/javamaster/models/user/Tutor.java
@@ -0,0 +1,30 @@
+package ru.javamaster.javamaster.models.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//TODO заглушка для класса InviteToken
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "tutors")
+public class Tutor {
+
+    @Id
+    @Column(name = "id")
+    Long id;
+
+    private RoleNameEnum role;
+
+    public Tutor(RoleNameEnum role) {
+        this.role = role;
+    }
+}
