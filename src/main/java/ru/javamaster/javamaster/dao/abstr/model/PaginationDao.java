a62acb7df684f5d468fb7b98260318087098fd57
Bolotov Alexander <distalker1@gmail.com>
2021-12-29 12:47:09 +0300

    fix-dao

--- src/main/java/ru/javamaster/javamaster/dao/abstr/model/PaginationDao.java
+++ src/main/java/ru/javamaster/javamaster/dao/abstr/model/PaginationDao.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
-public interface PaginationDao<K extends Serializable, T> extends ReadWriteDao<K, T>{
+public interface PaginationDao<K extends Serializable, T> extends ReadWriteDao<K, T> {
     List<T> getDataByNumberPage(int pageSize, int pageNumber);
     List<T> getDataByNumberPageAndFilterPattern(int pageSize, int pageNumber, String filterPattern);
     List<T> getDataByNumberPageAndFilterPatternAndParametersMap(int pageSize, int pageNumber, String filterPattern, Map<String, Object> parametersMap);
--- src/main/java/ru/javamaster/javamaster/dao/abstr/model/ReadWriteDao.java
+++ src/main/java/ru/javamaster/javamaster/dao/abstr/model/ReadWriteDao.java
@@ -3,16 +3,25 @@ package ru.javamaster.javamaster.dao.abstr.model;
 import java.io.Serializable;
 import java.util.Collection;
 
-public interface ReadWriteDao <K extends Serializable, T> extends ReadOnlyDao<K, T>{
+public interface ReadWriteDao<K extends Serializable, T> extends ReadOnlyDao<K, T> {
 
     void persist(T entity);
+
     void persistAll(T... entities);
-    void persistAll(Collection entities);
+
+    void persistAll(Collection<T> entities);
+
     T update(T e);
+
     void deleteWithCascadeIgnore(K id);
+
     void deleteWithCascadeEnable(K id);
+
     void deleteWithCascadeEnable(T entity);
-    void deleteAll(Collection entities);
+
+    void deleteAll(Collection<T> entities);
+
     void updateAll(Iterable<? extends T> entities);
-    void updateFieldById(String fieldName, String fieldValue, K id) ;
+
+    void updateFieldById(String fieldName, String fieldValue, long id);
 }
